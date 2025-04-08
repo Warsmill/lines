8dda02cc7bd2afe50982b905bed86f608ff8c4b9
warsmill <warsmill@yandex.ru>
2025-04-08 22:53:46 +0300

    first, second and third task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName + ".");
 
+        String fullNameUpperCase = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameUpperCase + ".");
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё","e").replace("Ё","E");
+        System.out.println(fullName);
     }
 }
